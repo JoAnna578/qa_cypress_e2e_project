207474a61a5f6fc9e99cd3744ee13172e4c9fd55
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-25 15:41:31 +0200

    Update article.cy.js

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,38 +1,71 @@
 /// <reference types='cypress' />
-/// <reference types='../support' />
-
-import ArticlePage from '../support/pages/ArticlePage';
+import ArticlePage from '../support/ArticlePage';
+import faker from 'faker';
+import SignInPage from '../support/pages/signIn.pageObject';
 
 describe('Article', () => {
   const articlePage = new ArticlePage();
-  const articleData = {
-    title: 'Test Article ' + Math.floor(Math.random() * 1000),
-    description: 'Test Description',
-    body: 'This is the body of the test article',
-    tags: ['test', 'cypress']
+  const signInPage = new SignInPage();
+  const user = {
+    email: 'testuser@example.com',
+    password: 'Password123'
   };
 
   before(() => {
-    articlePage.visitNewArticleForm();
+    cy.task('db:clear'); // czyścimy bazę danych
+    signInPage.visit();
+    signInPage.signIn(user.email, user.password);
   });
 
   beforeEach(() => {
-    cy.task('db:clear'); // czyścimy bazę przed każdym testem
+    cy.task('db:clear'); // każdy test startuje z czystą bazą
   });
 
-  it('should be created using New Article form', () => {
+  it('should create a new article', () => {
+    const articleData = {
+      title: faker.lorem.sentence(),
+      description: faker.lorem.sentence(),
+      body: faker.lorem.paragraph(),
+      tags: faker.lorem.words(3)
+    };
+
+    articlePage.visit(); // otwieramy formularz
     articlePage.createArticle(articleData);
-    articlePage.assertArticleCreated(articleData.title);
+    articlePage.assertArticleCreated(articleData);
   });
 
-  it('should be edited using Edit button', () => {
-    const updatedData = { ...articleData, body: 'Updated body content' };
+  it('should edit an article', () => {
+    const articleData = {
+      title: faker.lorem.sentence(),
+      description: faker.lorem.sentence(),
+      body: faker.lorem.paragraph(),
+      tags: faker.lorem.words(3)
+    };
+
+    articlePage.visit();
+    articlePage.createArticle(articleData);
+
+    const updatedData = {
+      ...articleData,
+      body: faker.lorem.paragraph()
+    };
+
     articlePage.searchArticle(articleData.title);
     articlePage.editArticle(updatedData);
     articlePage.assertArticleUpdated(updatedData.body);
   });
 
-  it('should be deleted using Delete button', () => {
+  it('should delete an article', () => {
+    const articleData = {
+      title: faker.lorem.sentence(),
+      description: faker.lorem.sentence(),
+      body: faker.lorem.paragraph(),
+      tags: faker.lorem.words(3)
+    };
+
+    articlePage.visit();
+    articlePage.createArticle(articleData);
+
     articlePage.searchArticle(articleData.title);
     articlePage.deleteArticle();
     articlePage.assertArticleDeleted(articleData.title);
