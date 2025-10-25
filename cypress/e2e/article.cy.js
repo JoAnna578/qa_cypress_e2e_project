9d73c57910530e19744dad58f42a5879ae1e25f3
JoAnna578 <joannakoloczek1@gmail.com>
2025-10-25 15:22:24 +0200

    Update article.cy.js

--- cypress/e2e/article.cy.js
+++ cypress/e2e/article.cy.js
@@ -1,24 +1,40 @@
 /// <reference types='cypress' />
 /// <reference types='../support' />
 
-describe('Article', () => {
-  before(() => {
+import ArticlePage from '../support/pages/ArticlePage';
 
+describe('Article', () => {
+  const articlePage = new ArticlePage();
+  const articleData = {
+    title: 'Test Article ' + Math.floor(Math.random() * 1000),
+    description: 'Test Description',
+    body: 'This is the body of the test article',
+    tags: ['test', 'cypress']
+  };
+
+  before(() => {
+    articlePage.visitNewArticleForm();
   });
 
   beforeEach(() => {
-    cy.task('db:clear');
+    cy.task('db:clear'); // czyścimy bazę przed każdym testem
   });
 
   it('should be created using New Article form', () => {
-
+    articlePage.createArticle(articleData);
+    articlePage.assertArticleCreated(articleData.title);
   });
 
   it('should be edited using Edit button', () => {
-
+    const updatedData = { ...articleData, body: 'Updated body content' };
+    articlePage.searchArticle(articleData.title);
+    articlePage.editArticle(updatedData);
+    articlePage.assertArticleUpdated(updatedData.body);
   });
 
   it('should be deleted using Delete button', () => {
-
+    articlePage.searchArticle(articleData.title);
+    articlePage.deleteArticle();
+    articlePage.assertArticleDeleted(articleData.title);
   });
 });
